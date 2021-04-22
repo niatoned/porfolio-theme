a5616fcad93eaab29b3d1f7f48c14f7da85b12be
niatoned <danieniat@gmail.com>
2021-04-21 22:45:40 -0400

    menus

---
+++ wp-content/themes/newtheme/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<?php wp_head();?>
+			<link rel="stylesheet" href="bootstrap.min.css">
+		</head>
+<body <?php body_class();?>>
+	
+
+	<header class="sticky-top">
+		
+		<?php wp_nav_menu(); 
+			array(
+				'theme_location' => 'top-menu',
+				'menu_class' => 'navigation'
+			)
+		?>
+	</header>
